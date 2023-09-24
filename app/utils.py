9032c0f277f57f1073f45ca3bd4de528268fe5c3
Justin Clark <jclarksummit@gmail.com>
2023-09-24 16:04:25 -0400

    Relocate functions for getting analytics codes to codes.py

--- app/utils.py
+++ app/utils.py
@@ -6,6 +6,8 @@ import re
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
 
+from app.codes import get_UA_code, get_GA_code, get_GTM_code
+
 # Semaphore to limit number of concurrent requests (10-15 appears to work fine. 20+ causes 443 error from web.archive.org)
 sem = asyncio.Semaphore(10)
 
@@ -67,77 +69,6 @@ def get_limit_from_frequency(frequency, start_date, end_date):
         )
 
     return None
-
-
-def get_UA_code(html):
-    """Returns UA codes (w/o duplicates) from given html, or None if not found.
-
-    Args:
-        html (str): Raw html.
-
-    Returns:
-        ["UA-12345678-1", "UA-12345678-2", ...]
-    """
-
-    # Regex pattern to find UA codes
-    pattern = re.compile("UA-\d[^\"|']*")
-
-    # Find all UA codes in html or return None
-    try:
-        UA_codes = pattern.findall(html)
-    except Exception as e:
-        print(e)
-        return None
-
-    # Remove duplicates and return
-    return list(set(UA_codes))
-
-
-def get_GA_code(html):
-    """Returns GA codes (w/o duplicates) from given html, or None if not found.
-
-    Args:
-        html (str): Raw html.
-
-    Returns:
-        ["G-1234567890", "G-1234567891", ...]
-    """
-
-    # Regex pattern to find GA codes
-    pattern = re.compile(r"G-[A-Za-z0-9]{10}")
-
-    # Find all GA codes in html or return None
-    try:
-        GA_codes = pattern.findall(html)
-    except Exception as e:
-        print(e)
-        return None
-
-    # Remove duplicates and return
-    return list(set(GA_codes))
-
-
-def get_GTM_code(html):
-    """Returns GTM codes (w/o duplicates) from given html, or None if not found.
-    Args:
-        html (str): Raw html.
-    Returns:
-        ["GTM-1234567890", "GTM-1234567891", ...]
-    """
-
-    # This pattern
-    pattern = re.compile(r"GTM-[A-Za-z0-9]{1,}")
-
-    # Find all GTM codes in html or return None
-    try:
-        GTM_codes = pattern.findall(html)
-    except Exception as e:
-        print(e)
-        return None
-
-    # Remove duplicates and return
-    return list(set(GTM_codes))
-
 
 def get_page_title(html):
     """Returns page title from given html, or None if not found."""
